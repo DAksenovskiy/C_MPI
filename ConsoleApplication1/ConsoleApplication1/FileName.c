b1eae00a8fe553c632cf194051fe304b9c906051
DAksenovskiy <den.aksyan12@gmail.com>
2024-05-26 20:45:16 +0300

    Stars FileName.c

--- ConsoleApplication1/ConsoleApplication1/FileName.c
+++ ConsoleApplication1/ConsoleApplication1/FileName.c
@@ -3,31 +3,31 @@
 #include <mpi.h>
 #include <time.h>       // for clock_t, clock(), CLOCKS_PER_SEC
 #include <omp.h>
+#include <locale.h>
+
+// Для множества звезд заданных координатами в трехмерной декартовой системе определить минимальную ограничивающую сферу
 
 //cd source\repos\ConsoleApplication1\x64\Debug
 //mpiexec -n 2 .\ConsoleApplication1.exe
 
-float vander_polynomial(float x0, float v0, float mu, float A, float w, int t[], int n)
+int main(float* argc, char** argv)
 {
-
-}
-
-int main(float *argc, char **argv)
-{
-
-    int x[10000] = {0};
-    int y[10000] = {0};
-    int z[10000] = {0};
-    float R[10000] = {0};
-    float R1[5000] = {0};
-    float R2[5000] = {0};
-
-    //Öåíòð ãàëàêòèêè
+    char* locale = setlocale(LC_ALL, "Russian");
+    //  SetConsoleCP(1251);
+    //  SetConsoleOutputCP(1251);
+    int x[10000] = { 0 };
+    int y[10000] = { 0 };
+    int z[10000] = { 0 };
+    float R[10000] = { 0 };
+    float R1[10000] = { 0 };
+    float R2[10000] = { 0 };
+    int n = 10000;
+    //Центр галактики
     x[0] = 0;
     y[0] = 0;
     z[0] = 0;
 
-    //Çâåçäû
+    //Звезды
     for (int i = 1; i < 10000; i++)
     {
         x[i] = rand() % 2000;
@@ -39,61 +39,69 @@ int main(float *argc, char **argv)
     int size = 0, rank = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     double start, end;
+    int k = n / size; //при size = 4 k = 2500
+
     MPI_Barrier(MPI_COMM_WORLD);
     start = MPI_Wtime();
-    //printf('%i', x[1]);
-    if (rank == 1)
-    {
-        #pragma omp parallel
-        {
-            for (int i = 5000; i < 10000; i++)
-            {
-                while ((pow(x[0] - x[i], 2) + pow(y[0] - y[i], 2) + pow(z[0] - z[i], 2)) > R2[i - 5000] * R2[i - 5000])
-                {
-                    R2[i - 5000] = R2[i - 5000] + 1;
-                }
-            }
-        }
-        MPI_Send(&R2, 5000, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-    }
-    else if (rank == 0) {
 
-        #pragma omp parallel
+    for (int j = 1; j < size - 1; j++) {
+        if (rank == j)
         {
-            for (int i = 5000; i < 10000; i++)
+            for (int i = k * j; i < k * j + 1; i++)
             {
                 while ((pow(x[0] - x[i], 2) + pow(y[0] - y[i], 2) + pow(z[0] - z[i], 2)) > R1[i] * R1[i])
                 {
                     R1[i] = R1[i] + 1;
                 }
             }
+            MPI_Send(&R1, 10000, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
         }
-        MPI_Status status;
-        MPI_Recv(&R2, 5000, MPI_FLOAT, 1, 0, MPI_COMM_WORLD, &status);
+    }
 
-        for (int i = 0; i < 5000; i++)
+    if (rank == 0) {
+        for (int i = 0; i < k; i++)
+        {
+            while ((pow(x[0] - x[i], 2) + pow(y[0] - y[i], 2) + pow(z[0] - z[i], 2)) > R1[i] * R1[i])
+            {
+                R1[i] = R1[i] + 1;
+            }
+        }
+        for (int i = 0; i < k; i++)
         {
             R[i] = R1[i];
         }
-        for (int i = 5000; i < 10000; i++)
-        {
-            R[i] = R2[i-5000];
-        }
-        float Max = 0;
-        for (int i = 0; i < 10000; i++)
-        {
-            if (R[i] > Max)
+        MPI_Status status;
+        for (int j = 1; j < size - 1; j++) {
+
+            MPI_Recv(&R2, 10000, MPI_FLOAT, j, 0, MPI_COMM_WORLD, &status);
+            for (int i = 0; i < k; i++)
             {
-                Max = R[i];
+                R[i + k * j] = R2[i];
             }
         }
-        end = MPI_Wtime();
-        printf("\nMax = %f\n", Max);
-        printf("\nRuntime = %f\n", end - start);
-       // printf('%i', x[1]);
-
     }
+
+
+    float Max = 0;
+    for (int i = 0; i < 10000; i++)
+    {
+        if (R[i] > Max)
+        {
+            Max = R[i];
+        }
+    }
+    end = MPI_Wtime();
+    if (rank == 0) {
+        //printf("\nДля множества звезд заданных координатами в трехмерной декартовой системе определить минимальную ограничивающую сферу\n");
+        printf("\n\n\n\n");
+        printf("\nR = %f\n", Max); //R минимальной ограничивающей сферы
+        printf("\ntime = %f\n", end - start);
+        printf("\n\n\n\n");
+        // printf('%i', x[1]);
+    }
+
     MPI_Finalize();
     return 0;
 }
